630abf1bf40557dddfffccafdd5fe24354221294
Dmytro <dima.shevchenko10@gmail.com>
2025-12-04 15:23:04 +0200

    feat(comments): validate allowed html tags

--- Discussion.Core/DTO/Comment/AddComment/AddCommentRequestDTOValidator.cs
+++ Discussion.Core/DTO/Comment/AddComment/AddCommentRequestDTOValidator.cs
@@ -23,6 +23,53 @@ public class AddCommentRequestDTOValidator : AbstractValidator<AddCommentRequest
             .MaximumLength(2000)
             .WithMessage("Comment cannot exceed 2000 characters.")
             .Must(XhtmlValidator.IsValidXhtml)
-            .WithMessage("Comment contains invalid HTML/XHTML");;
+            .WithMessage("Comment contains invalid HTML/XHTML")
+            .Must(ContainsOnlyAllowedHtml)
+            .WithMessage("Comment contains invalid or not allowed HTML.");;
+    }
+    
+    private static readonly HashSet<string> AllowedTags = new(StringComparer.OrdinalIgnoreCase)
+    {
+        "a", "code", "i", "strong"
+    };
+
+    private bool ContainsOnlyAllowedHtml(string? text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+            return true;
+
+        var doc = new HtmlAgilityPack.HtmlDocument();
+        doc.LoadHtml(text);
+
+        foreach (var node in doc.DocumentNode.Descendants())
+        {
+            if (node.NodeType != HtmlAgilityPack.HtmlNodeType.Element)
+            {
+                continue;
+            }
+            
+            if (!AllowedTags.Contains(node.Name))
+            {
+                return false;
+            }
+            
+            if (node.Name.Equals("a", StringComparison.OrdinalIgnoreCase))
+            {
+                var href = node.GetAttributeValue("href", null);
+                if (string.IsNullOrWhiteSpace(href)) return false;
+
+                var title = node.GetAttributeValue("title", null);
+                if (string.IsNullOrWhiteSpace(title)) return false;
+                
+            }
+            
+            if (node.Attributes.Any(a =>
+                    node.Name != "a" || a.Name != "href" && a.Name != "title"))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
